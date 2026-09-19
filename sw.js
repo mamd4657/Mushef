ce9f022d0b5322507835ffa89036b23afcdc083c
mamd4657 <ma46md57@gmail.com>
2026-09-19 17:19:09 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'mushaf-cache-v4';
+const APP_SHELL = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(() => {}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const network = fetch(event.request).then((resp) => {
+        if (resp && resp.ok) { const clone = resp.clone(); caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone)); }
+        return resp;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
