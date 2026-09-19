65de8f6f91827b37c0845b19267f80ac7f0e2bc1
mamd4657 <ma46md57@gmail.com>
2026-09-19 15:57:39 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mushaf-cache-v3';
+const CACHE_NAME = 'mushaf-cache-v4';
 const APP_SHELL = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(() => {}));
